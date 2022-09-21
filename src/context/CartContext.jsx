59a3862d359505f46156ff97959ba643418da835
facundo brondo <facundobrondo34@gmail.com>
2022-09-21 17:33:44 -0300

    en pausa

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -18,6 +18,11 @@ const CartProvider = ({ children }) => {
         }
     }
 
+    const totalPrice = () => {
+        return cart.reduce((prev, act) => prev + act.quantity * act.price, 0);
+    }
+    const totalProducts= () => cart.reduce((acumulador, productoActual) => acumulador + productoActual,quantity, 0);
+
     const clearCart = () => setCart([]);
 
     const isInCart = (id) => cart.find(product => product.id === id) ? true :false
