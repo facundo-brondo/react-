c85d1ce3412cd13ca270ba758c449f217bd9e1be
facundo brondo <facundobrondo34@gmail.com>
2022-09-21 18:01:40 -0300

    cartviwe

--- src/components/CardWidget/index.jsx
+++ src/components/CardWidget/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useCartContext } from '../../context/CartContext';
 
 export const CardWidget = () => {
+  const { totalProducts } = useCartContext();
   return (
-    <i className="bi bi-cart3"></i>
+    <> 
+     <i className="bi bi-cart3"></i>
+     <span>{totalProducts() || ''}</span>
+    </>
+   
   );
 }
 export default CardWidget;
